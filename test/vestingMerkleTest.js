e133b7a566f9aa94930cb69664d14f2baaed1579
Abe Othman <aothman@cs.cmu.edu>
2022-09-27 09:06:01 -0700

    Passes linter tests

--- test/vestingMerkleTest.js
+++ test/vestingMerkleTest.js
@@ -1,4 +1,5 @@
-const helpers = require("@nomicfoundation/hardhat-network-helpers");
+/* eslint-disable new-cap */
+const helpers = require('@nomicfoundation/hardhat-network-helpers');
 const { expect } = require('chai');
 const { BigNumber } = require('ethers');
 const erc20 = require('./abis/erc20.abi');
@@ -16,13 +17,11 @@ describe('Vesting Merkle Tests', function() {
 
   let snapshot;
 
-  const _1e18 = '000000000000000000';
+  const AUGUR_REP_ADDRESS = '0x221657776846890989a759BA2973e427DfF5C9bB';
+  const RICH_GUY_ADDRESS = '0x28C6c06298d514Db089934071355E5743bf21d60';
 
-  const AUGUR_REP_ADDRESS = "0x221657776846890989a759BA2973e427DfF5C9bB";
-  const RICH_GUY_ADDRESS = "0x28C6c06298d514Db089934071355E5743bf21d60";
-
-  const RECIPIENT_ZERO_ADDRESS = "0x02Da682238ff9D89f1974653f33aD6A679642B95";
-  const RECIPIENT_ONE_ADDRESS = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+  const RECIPIENT_ZERO_ADDRESS = '0x02Da682238ff9D89f1974653f33aD6A679642B95';
+  const RECIPIENT_ONE_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
 
   async function sendInAsset(contractAddress) {
     richGuyAugur = await ethers.getContractAt(erc20, AUGUR_REP_ADDRESS, richGuy);
@@ -34,8 +33,8 @@ describe('Vesting Merkle Tests', function() {
     vmdFactory = await ethers.getContractFactory('VestingMerkleDistributor');
     merkleObject = JSON.parse(fs.readFileSync('scripts/example_output.json'));
 
-    claimZero = merkleObject["claims"][RECIPIENT_ZERO_ADDRESS];
-    claimOne = merkleObject["claims"][RECIPIENT_ONE_ADDRESS];
+    claimZero = merkleObject['claims'][RECIPIENT_ZERO_ADDRESS];
+    claimOne = merkleObject['claims'][RECIPIENT_ONE_ADDRESS];
 
     await hre.network.provider.request({
       method: 'hardhat_impersonateAccount',
@@ -52,30 +51,29 @@ describe('Vesting Merkle Tests', function() {
   });
 
   it('Deployment tests', async function() {
-    let timestamp = await helpers.time.latest();
+    const timestamp = await helpers.time.latest();
 
-    await expect(vmdFactory.deploy(AUGUR_REP_ADDRESS, merkleObject["merkleRoot"], timestamp+1, timestamp)).to.be.revertedWith("Invalid interval");
+    await expect(vmdFactory.deploy(AUGUR_REP_ADDRESS, merkleObject['merkleRoot'], timestamp+1, timestamp)).to.be.revertedWith('Invalid interval');
 
 
-    const vmd = await vmdFactory.deploy(AUGUR_REP_ADDRESS, merkleObject["merkleRoot"], timestamp, timestamp);
+    const vmd = await vmdFactory.deploy(AUGUR_REP_ADDRESS, merkleObject['merkleRoot'], timestamp, timestamp);
     await vmd.deployed();
 
-    expect(await vmd.MERKLE_ROOT()).to.equal(merkleObject["merkleRoot"]);
+    expect(await vmd.MERKLE_ROOT()).to.equal(merkleObject['merkleRoot']);
     expect(await vmd.TOKEN_ADDRESS()).to.equal(AUGUR_REP_ADDRESS);
     expect(await vmd.getVestingStartAndEnd()+'').to.equal(timestamp+','+timestamp);
-  
   });
 
   it('Cannot withdraw with a bad proof or before specified interval ', async function() {
-    let futureTimestamp = await helpers.time.latest()+10000;
+    const futureTimestamp = await helpers.time.latest()+10000;
 
-    const vmd = await vmdFactory.deploy(AUGUR_REP_ADDRESS, merkleObject["merkleRoot"], futureTimestamp, futureTimestamp);
+    const vmd = await vmdFactory.deploy(AUGUR_REP_ADDRESS, merkleObject['merkleRoot'], futureTimestamp, futureTimestamp);
     await vmd.deployed();
 
     expect(await vmd.fractionVested()).to.equal(0);
 
     // claimVested(uint256 index, address account, uint256 tokenGrant, bytes32[] calldata merkleProof)
-    
+
     let tx = vmd.claimVested(1, RECIPIENT_ONE_ADDRESS, claimZero.amount, claimOne.proof);
     await expect(tx).to.be.revertedWith('Invalid proof');
 
@@ -92,7 +90,7 @@ describe('Vesting Merkle Tests', function() {
   it('Can fully withdraw after end of vesting', async function() {
     const pastTimestamp = await helpers.time.latest()-10000;
 
-    const vmd = await vmdFactory.deploy(AUGUR_REP_ADDRESS, merkleObject["merkleRoot"], pastTimestamp, pastTimestamp);
+    const vmd = await vmdFactory.deploy(AUGUR_REP_ADDRESS, merkleObject['merkleRoot'], pastTimestamp, pastTimestamp);
     await vmd.deployed();
 
     await sendInAsset(vmd.address);
@@ -100,7 +98,7 @@ describe('Vesting Merkle Tests', function() {
     expect(await vmd.fractionVested()).to.equal(255);
 
     // claimVested(uint256 index, address account, uint256 tokenGrant, bytes32[] calldata merkleProof)
-    
+
     let tx = vmd.claimVested(1, RECIPIENT_ONE_ADDRESS, claimOne.amount, claimOne.proof);
     await expect(tx).to.emit(vmd, 'Claimed').withArgs(RECIPIENT_ONE_ADDRESS, claimOne.amount);
     expect(await richGuyAugur.balanceOf(RECIPIENT_ONE_ADDRESS)).to.equal(BN.from(claimOne.amount));
@@ -128,7 +126,7 @@ describe('Vesting Merkle Tests', function() {
     const pastTimestamp = await helpers.time.latest()-10000;
     const futureTimestamp = pastTimestamp + 50000;
 
-    const vmd = await vmdFactory.deploy(AUGUR_REP_ADDRESS, merkleObject["merkleRoot"], pastTimestamp, futureTimestamp);
+    const vmd = await vmdFactory.deploy(AUGUR_REP_ADDRESS, merkleObject['merkleRoot'], pastTimestamp, futureTimestamp);
     await vmd.deployed();
 
     await sendInAsset(vmd.address);
@@ -161,10 +159,5 @@ describe('Vesting Merkle Tests', function() {
     expect(await vmd.getClaimAmount(0)).to.equal(102);
     expect(await vmd.getClaimAmount(1)).to.equal(102);
     expect(await vmd.getClaimAmount(2)).to.equal(0);
-
-
-
   });
-
-
 });
